58dafb2bb1b3a87961d7ebede59163780efca38f
articd3vtemp <ad3vtemp@gmail.com>
2024-12-08 19:51:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if st.button("Start Streaming"):
     st.write("Starting the stream...")
     # os.system("ffmpeg -stream_loop -1 -re -i room.mp4 -stream_loop -1 -re -i https://stream.zeno.fm/6hszhrzidmcuv -vcodec libx264 -pix_fmt yuvj420p -maxrate 512k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 98k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150k -f flv  rtmp://a.rtmp.youtube.com/live2/qp0u-wb9b-3ptq-adxz-9rdj")
     os.system("pip install ffmpeg-python")
-    os.system("wget")
+    os.system("ls")
     os.system("curl")
     # ffmpeg_path = "/home/appuser/.local/bin"  # Path to the local FFmpeg binary
     st.write("Streaming started!")
