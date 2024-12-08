72f7070a8909b43d8f652b676e6e19dc12ab3f65
articd3vtemp <ad3vtemp@gmail.com>
2024-12-08 14:01:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
-from keep_alive import keep_alive
-# os.system("chmod +x yt.sh")
-# os.system("bash yt.sh")
-os.system("ffmpeg -stream_loop -1 -re -i room.mp4 -stream_loop -1 -re -i https://stream.zeno.fm/6hszhrzidmcuv -vcodec libx264 -pix_fmt yuvj420p -maxrate 512k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 98k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150k -f flv  rtmp://a.rtmp.youtube.com/live2/qp0u-wb9b-3ptq-adxz-9rdj")
-keep_alive()  # Starts a webserver to be pinged.
+import streamlit as st
+
+# Streamlit UI
+st.title("YouTube RTMP Streamer")
+st.write("Stream a video with an audio source to YouTube Live.")
+
+if st.button("Start Streaming"):
+    st.write("Starting the stream...")
+    os.system("ffmpeg -stream_loop -1 -re -i room.mp4 -stream_loop -1 -re -i https://stream.zeno.fm/6hszhrzidmcuv -vcodec libx264 -pix_fmt yuvj420p -maxrate 512k -preset veryfast -r 12 -framerate 30 -g 50 -c:a aac -b:a 98k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150k -f flv  rtmp://a.rtmp.youtube.com/live2/qp0u-wb9b-3ptq-adxz-9rdj")
+    st.write("Streaming started!")
