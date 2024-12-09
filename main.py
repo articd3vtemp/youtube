87e1eb58b26621b0a54335b35a5218a847371a80
articd3vtemp <ad3vtemp@gmail.com>
2024-12-09 16:04:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ if st.button("Start Streaming"):
     # Path to ffmpeg binary
     ffmpeg_path = "./ffmpeg-7.0.2-amd64-static/ffmpeg"
     stream_command = (
-        f"{ffmpeg_path} -stream_loop -1 -re -i room.mp4 -stream_loop -1 -re -i https://stream.zeno.fm/6hszhrzidmcuv "
+        f"{ffmpeg_path} -stream_loop -1 -re -i room.mp4 -stream_loop -1 -re -i https://stream.zeno.fm/9kaed9hws98uv "
         f"-vcodec libx264 -pix_fmt yuvj420p -maxrate 512k -preset veryfast -r 12 -framerate 30 -g 50 "
         f"-c:a aac -b:a 98k -ar 44100 -strict experimental -video_track_timescale 1000 -b:v 150k -f flv "
         f"rtmp://a.rtmp.youtube.com/live2/qp0u-wb9b-3ptq-adxz-9rdj"
